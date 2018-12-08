563e0d6bd5e303e861f3b4ea387be9fbca808756
eddie <appositum@protonmail.com>
2018-12-08 01:22:43 -0300

    Change vector cons operator and add proper exports

--- src/Stroots/Vector.hs
+++ src/Stroots/Vector.hs
@@ -5,24 +5,28 @@
 {-# LANGUAGE TypeOperators        #-}
 {-# LANGUAGE UndecidableInstances #-}
 
-module Stroots.Vector where
+module Stroots.Vector
+  ( append
+  , flatten
+  , zip
+  , zipWith
+  ) where
 
 import Prelude hiding (zip, zipWith)
-import Control.Applicative (liftA2)
 import Data.Kind (Type)
 import Stroots.Nat
 
-infixr 5 :-
+infixr 5 :>
 data Vector :: Nat -> Type -> Type where
   Nil  :: Vector 'Z a
-  (:-) :: a -> Vector n a -> Vector ('S n) a
+  (:>) :: a -> Vector n a -> Vector ('S n) a
 
 deriving instance Show a => Show (Vector n a)
 
 instance Functor (Vector n) where
   fmap :: (a -> b) -> Vector n a -> Vector n b
   fmap _ Nil = Nil
-  fmap f (x:-xs) = f x :- fmap f xs
+  fmap f (x:>xs) = f x :> fmap f xs
 
 instance Applicative (Vector 'Z) where
   pure :: a -> Vector 'Z a
@@ -33,10 +37,10 @@ instance Applicative (Vector 'Z) where
 
 instance Applicative (Vector n) => Applicative (Vector ('S n)) where
   pure :: a -> Vector ('S n) a
-  pure a = a :- pure a
+  pure a = a :> pure a
 
   (<*>) :: Vector ('S n) (a -> b) -> Vector ('S n) a -> Vector ('S n) b
-  (f:-fs) <*> (x:-xs) = f x :- (fs <*> xs)
+  (f:>fs) <*> (x:>xs) = f x :> (fs <*> xs)
 
 instance Monad (Vector 'Z) where
   (>>=) :: Vector 'Z a -> (a -> Vector 'Z b) -> Vector 'Z b
@@ -49,30 +53,30 @@ instance Monad (Vector n) => Monad (Vector ('S n)) where
 instance Foldable (Vector n) where
   foldr :: (a -> b -> b) -> b -> Vector n a -> b
   foldr _ z Nil = z
-  foldr f z (x:-xs) = f x (foldr f z xs)
+  foldr f z (x:>xs) = f x (foldr f z xs)
 
 instance Traversable (Vector n) where
   sequenceA :: Applicative f => Vector n (f a) -> f (Vector n a)
   sequenceA Nil = pure Nil
-  sequenceA (x:-xs) = (:-) <$> x <*> sequenceA xs
-
-empty :: Vector 'Z a
-empty = Nil
+  sequenceA (x:>xs) = (:>) <$> x <*> sequenceA xs
 
 append :: Vector n a -> Vector m a -> Vector (n + m) a
 append Nil ys = ys
-append (x:-xs) ys = x :- append xs ys
+append (x:>xs) ys = x :> append xs ys
 
 zipWith :: (a -> b -> c) -> Vector n a -> Vector n b -> Vector n c
 zipWith _ Nil Nil = Nil
-zipWith f (x:-xs) (y:-ys) = f x y :- zipWith f xs ys
+zipWith f (x:>xs) (y:>ys) = f x y :> zipWith f xs ys
 
 zip :: Vector n a -> Vector n b -> Vector n (a, b)
 zip Nil Nil = Nil
-zip (x:-xs) (y:-ys) = (x, y) :- zip xs ys
+zip (x:>xs) (y:>ys) = (x, y) :> zip xs ys
 
 diagonal :: Vector n (Vector n a) -> Vector n a
 diagonal Nil = Nil
-diagonal ((x:-xs):-xss) = x :- diagonal (tl <$> xss) where
+diagonal ((x:>xs):>xss) = x :> diagonal (tl <$> xss) where
   tl :: Vector ('S n) a -> Vector n a
-  tl (_:-xs) = xs
+  tl (_:>xs) = xs
+
+flatten :: Vector n (Vector n a) -> Vector n a
+flatten = diagonal
