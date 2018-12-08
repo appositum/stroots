048e12b489b8cfbda49296c5fd71c209f7b00513
eddie <appositum@protonmail.com>
2018-12-08 00:58:16 -0300

    Change list operator and use GADTs

--- src/Stroots/List.hs
+++ src/Stroots/List.hs
@@ -1,17 +1,26 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE GADTs              #-}
+{-# LANGUAGE KindSignatures     #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeFamilies       #-}
 
 module Stroots.List where
 
+import Data.Kind (Type)
 import GHC.Exts (fromList, toList, IsList, Item)
 import Control.Applicative (empty, Alternative, (<|>))
 
-infixr 5 :>
-data List a = Nil | a :> List a deriving (Eq, Show)
+infixr 5 :|
+data List :: Type -> Type where
+  Nil  :: List a
+  (:|) :: a -> List a -> List a
+
+deriving instance Eq a => Eq (List a)
+deriving instance Show a => Show (List a)
 
 instance Semigroup (List a) where
   (<>) :: List a -> List a -> List a
   Nil <> ys = ys
-  (x:>xs) <> ys = x :> (xs <> ys)
+  (x:|xs) <> ys = x :| (xs <> ys)
 
 instance Monoid (List a) where
   mempty :: List a
@@ -20,15 +29,15 @@ instance Monoid (List a) where
 instance Functor List where
   fmap :: (a -> b) -> List a -> List b
   fmap _ Nil = Nil
-  fmap f (x:>xs) = f x :> fmap f xs
+  fmap f (x:|xs) = f x :| fmap f xs
 
 instance Applicative List where
   pure :: a -> List a
-  pure a = a :> Nil
+  pure a = a :| Nil
 
   (<*>) :: List (a -> b) -> List a -> List b
   Nil <*> _ = Nil
-  (f:>fs) <*> xs = (f <$> xs) <> (fs <*> xs)
+  (f:|fs) <*> xs = (f <$> xs) <> (fs <*> xs)
 
 instance Alternative List where
   empty :: List a
@@ -45,22 +54,22 @@ instance Monad List where
 instance Foldable List where
   foldr :: (a -> b -> b) -> b -> List a -> b
   foldr _ z Nil = z
-  foldr f z (x:>xs) = f x (foldr f z xs)
+  foldr f z (x:|xs) = f x (foldr f z xs)
 
 instance Traversable List where
   sequenceA :: Applicative f => List (f a) -> f (List a)
   sequenceA Nil = pure Nil
-  sequenceA (x:>xs) = (:>) <$> x <*> sequenceA xs
+  sequenceA (x:|xs) = (:|) <$> x <*> sequenceA xs
 
 instance IsList (List a) where
   type Item (List a) = a
   fromList :: [a] -> List a
   fromList [] = Nil
-  fromList (x:xs) = x :> fromList xs
+  fromList (x:xs) = x :| fromList xs
 
   toList :: List a -> [a]
   toList Nil = []
-  toList (x:>xs) = x : toList xs
+  toList (x:|xs) = x : toList xs
 
 empty :: List a
 empty = Nil
