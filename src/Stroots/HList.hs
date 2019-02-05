ad6c0fa973da95dd3740f7f2fad6390ae3477dfd
eddie <appositum@protonmail.com>
2019-02-05 16:15:12 -0300

    Update HList.hs

--- src/Stroots/HList.hs
+++ src/Stroots/HList.hs
@@ -4,16 +4,17 @@ module Stroots.HList
 
 import Data.Kind
 import Stroots.Nat
+import Stroots.Vector
 
 infixr 6 :::
-data HList :: [Type] -> Type where
-  HNil  :: HList '[]
-  (:::) :: a -> HList as -> HList (a ': as)
+data HList :: Vector n Type -> Type where
+  HNil  :: HList 'Nil
+  (:::) :: a -> HList as -> HList (a ':> as)
 
-instance Show (HList '[]) where
-  show :: HList '[] -> String
+instance Show (HList 'Nil) where
+  show :: HList 'Nil -> String
   show HNil = "HNil"
 
-instance (Show (HList as), Show a) => Show (HList (a ': as)) where
-  show :: HList (a ': as) -> String
+instance (Show (HList as), Show a) => Show (HList (a ':> as)) where
+  show :: HList (a ':> as) -> String
   show (a:::as) = show a ++ " ::: " ++ show as
